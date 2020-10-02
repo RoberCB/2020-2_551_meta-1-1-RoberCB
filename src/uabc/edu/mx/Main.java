a1481755f70eddf1644deb4ecda9d2494c676f5d
Roberto Cruz Betancourt <roberto.cruz.betancourt@uabc.edu.mx>
2020-10-02 07:02:19 -0700

    Se agrego otra instrucción

--- src/uabc/edu/mx/Main.java
+++ src/uabc/edu/mx/Main.java
@@ -3,6 +3,8 @@ package uabc.edu.mx;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("---------------------META 1.1---------------------");
         System.out.println("Hola mundo!");
+        System.out.println("Roberto Cruz Betancourt");
     }
 }
